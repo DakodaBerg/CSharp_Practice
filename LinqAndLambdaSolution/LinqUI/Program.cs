221b9b40b67dfff6fc882c7ab32c4cae124e9589
Dakoda Berg <dakodaberg97@gmail.com>
2024-05-05 02:37:04 -0400

    Linq and Lambda

---
+++ LinqAndLambdaSolution/LinqUI/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace LinqUI
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Done");
+            Console.ReadLine();
+        }
+
+        private static void LambdaTests()
+        {
+            var data = SampleData.GetContactData();
+
+            var results = data.Where(x => x.Addresses.Count > 1);
+        }
+    }
+}
---
+++ LinqAndLambdaSolution/LinqUI/SampleData.cs
@@ -0,0 +1,37 @@
+﻿using LinqUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinqUI
+{
+    public class SampleData
+    {
+        public static List<ContactModel> GetContactData()
+        {
+            List<ContactModel> ouput = new List<ContactModel>
+            {
+                new ContactModel{Id = 1,FirstName = "crystal",LastName = "_test_",Addresses = new List<int> {2}},
+                new ContactModel{Id = 2,FirstName = "megan",LastName = "berg",Addresses = new List<int> {1,2,3,4}},
+                new ContactModel{Id = 3,FirstName = "ryan",LastName = "james",Addresses = new List<int> {1,2,4}},
+                new ContactModel{Id = 4,FirstName = "dakoda",LastName = "smith",Addresses = new List<int> {2,5}},
+                new ContactModel{Id = 5,FirstName = "jeff",LastName = "a",Addresses = new List<int> {5}},
+                new ContactModel{Id = 6,FirstName = "john",LastName = "aegr",Addresses = new List<int> {1}}
+            };
+            return ouput;
+        }
+
+        public static List<AddressModel> GetAddressData()
+        {
+            List<AddressModel> output = new List<AddressModel>()
+            {
+                new AddressModel{Id = 1, City = "bristol",State = "FL"},
+                new AddressModel{Id = 2, City = "dothan",State = "AL"},
+                new AddressModel{Id = 3, City = "austin",State = "TX"},
+                new AddressModel{Id = 4, City = "atlanta",State = "GA"},
+                new AddressModel{Id = 5, City = "New york",State = "NY"}
+            };
+            return output;
+        }
+    }
+}
