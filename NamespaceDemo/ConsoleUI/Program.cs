ea25929daa4a11ff70d59e1234a477dbbab50c9f
Dakoda Berg <dakodaberg97@gmail.com>
2024-05-05 02:40:09 -0400

    NameSpace

---
+++ NamespaceDemo/ConsoleUI/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using ConsoleUI.Models;
+using FoundationInfo;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PersonModel person = new PersonModel();
+
+            List<PersonModel> list = new List<PersonModel>();
+
+            //Changed namespace
+            //Calculations.add(10, 12);
+
+            //full name
+            //ConsoleUI.Calculations.add(20, 13);
+
+            //using statments allows me not to have to type it all out
+            FoundationInfo.Calculations.add(10, 12);
+
+            Calculations.add(4, 3);
+
+            Console.ReadLine();
+        }
+    }
+}
