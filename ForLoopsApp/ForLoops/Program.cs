54afba322bb20ee82e97855853ed3945e2d88d77
Dakoda Berg <dakodaberg97@gmail.com>
2024-05-05 02:31:57 -0400

    For Loops

---
+++ ForLoopsApp/ForLoops/Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+//for (int i = 0; i < 10; i++)
+//{
+//    Console.WriteLine($"The value of i is {i}");
+//}
+
+
+//string data = "dakoda,ryan,john,bob,sue";
+//List<string> firstName = data.Split(',').ToList();
+
+//for (int i = 0; i < firstName.Count; i++)
+//{
+//    Console.WriteLine($"{firstName[i]} is a student");
+//}
+
+List<decimal> charges = new();
+
+charges.Add(23.78M);
+charges.Add(42343245.3424M);
+charges.Add(32M);
+
+decimal total = 0;
+
+for (int i = 0; i < charges.Count; i++)
+{
+    total += charges[i];
+}
+
+Console.WriteLine($"Are total charge is ${total}");
---
+++ ForLoopsHomeWorkApp/ForLoopsHomeWork/Program.cs
@@ -0,0 +1,16 @@
+﻿
+//HomeWork
+//Ask the user for a conmma-separated list of first names(no spaces).
+//Split the string into string array.
+//Loop through the array and print "Hello <name>" to the console for each person.
+
+Console.WriteLine("Please enter a list of first names(conmma-separated & no spaces):");
+string data = Console.ReadLine();
+string[] firstNames = data.Split(",".ToArray());
+
+
+for (int i = 0; i < firstNames.Length; i++)
+{
+    Console.WriteLine($"Hello {firstNames[i]}");
+}
+
