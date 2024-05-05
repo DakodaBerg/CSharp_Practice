073e0de25cb857c6b9a03cf9f5b25f5cfdd2fd2c
Dakoda Berg <dakodaberg97@gmail.com>
2024-05-05 01:39:56 -0400

    Blazor Message Wall

---
+++ BlazorServerMessageWallApp/BlazorServerMessageWall/Models/MessageModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorServerMessageWall.Models;
+
+
+public class MessageModel
+{
+    [Required]
+    public string? Message { get; set; } 
+
+}
