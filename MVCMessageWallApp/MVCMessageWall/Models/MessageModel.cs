5962b345bb2290cbed3f189f004f55512c5e8fff
Dakoda Berg <dakodaberg97@gmail.com>
2024-05-05 02:39:36 -0400

    Message Wall

---
+++ MVCMessageWallApp/MVCMessageWall/Models/MessageModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVCMessageWall.Models
+{
+    public class MessageModel
+    {
+        [Required]
+        [StringLength(10, MinimumLength = 3)]
+        [Display(Name = "User Message")]
+        public string Message { get; set; }
+    }
+}
