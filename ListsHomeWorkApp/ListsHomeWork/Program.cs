d9b1360b3ca8ee779676526aeee4a0444638eb7d
Dakoda Berg <dakodaberg97@gmail.com>
2024-05-05 02:37:47 -0400

    List

---
+++ ListsHomeWorkApp/ListsHomeWork/Program.cs
@@ -0,0 +1,46 @@
+﻿//HomeWork
+//Add students to a class roster List
+//until there are no more students.
+//Then print out the count of the students to the console.
+
+List<string> classRoster = new List<string>();
+
+//Console.Write("Will you like to add a Students name to the roster? (Yes/No): ");
+//string? yesOrNo = Console.ReadLine();
+
+//if (yesOrNo.ToLower() == "no")
+//{
+//    Console.WriteLine($"There are {classRoster.Count} students added to the roster");
+//}
+//else
+//{
+//    Console.WriteLine("Please enter the name of the student you want to add: ");
+//    string? newStudent = Console.ReadLine();
+//    classRoster.Add(newStudent);
+//    Console.WriteLine($"{classRoster.Count}");
+//}
+
+
+do
+{
+    Console.Write("Will you like to add a Students name to the roster? (Yes/No/Exit): ");
+    string? yesOrNo = Console.ReadLine();
+
+    if (yesOrNo.ToLower() == "exit")
+    {
+        break;
+    }
+    if (yesOrNo.ToLower() == "no")
+    {
+        Console.WriteLine($"There are {classRoster.Count} students added to the roster");
+    }
+    else
+    {
+        Console.WriteLine("Please enter the name of the student you want to add: ");
+        string? newStudent = Console.ReadLine();
+        classRoster.Add(newStudent);
+        Console.WriteLine($"{classRoster.Count}");
+    }
+
+
+} while (true);
---
+++ listsApp/lists/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+List<string> firstNames = new List<string>();
+
+firstNames.Add("Dakoda");
+firstNames.Add("John");
+firstNames.Add("Megan");
+firstNames.Add("Tim");
+firstNames.Add("Ryan");
+firstNames.Add("Frank");
+
+Console.WriteLine(firstNames[firstNames.Count - 1]);
+
+
+List<int> ages = new List<int>();
+ages.Add(1);
+ages.Add(2);
+ages.Add(3);
+ages.Add(4);
+
+// List<T> - T is a generic can pick the Type
+
+string data = "berg,hill,smith";
+List<string> lastNames = data.Split(',').ToList();
+lastNames.Add("wong");
