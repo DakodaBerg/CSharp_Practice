7f7a742af71498b509aff7406f130b638ead2a49
Dakoda Berg <dakodaberg97@gmail.com>
2024-05-05 01:38:43 -0400

    If Statements

---
+++ BasicIfApp/BasicIf/Program.cs
@@ -0,0 +1,36 @@
+﻿
+
+//bool isComplete = false;
+
+//if (isComplete)
+//{
+//    Console.WriteLine("The statement was true.");
+//}
+//else
+//{
+//    Console.WriteLine("The statement was false.");
+//}
+
+
+Console.Write("What is your first name: ");
+string? firstName = Console.ReadLine();
+
+
+if (firstName.ToLower() == "d akoda")
+{
+    Console.WriteLine("Hello Dakoda!");
+}
+else
+{
+    Console.WriteLine($"Hi {firstName}");
+}
+
+
+
+
+
+
+Console.WriteLine("End of program.");
+
+
+
