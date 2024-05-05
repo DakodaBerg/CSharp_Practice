006ec5292aaff3cacd47aefb750815fc58078589
Dakoda Berg <dakodaberg97@gmail.com>
2024-05-05 02:40:24 -0400

    NoSql

---
+++ NoSqlDBSolution/ApiDBUI/Pages/Index.cshtml.cs
@@ -0,0 +1,67 @@
+﻿using ApiDBUI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ApiDBUI.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public IndexModel(ILogger<IndexModel> logger, IHttpClientFactory httpClientFactory)
+        {
+            _logger = logger;
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task OnGet()
+        {
+            await CreateContact();
+            await GetAllContacts();
+        }
+
+        private async Task GetAllContacts()
+        {
+            var _client = _httpClientFactory.CreateClient();
+            var response = await _client.GetAsync("https://localhost:44379/api/contacts");
+
+            List<ContactModel> contacts;
+            if (response.IsSuccessStatusCode)
+            {
+                var options = new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true,
+                };
+                string responseText = await response.Content.ReadAsStringAsync();
+                contacts = JsonSerializer.Deserialize<List<ContactModel>>(responseText, options);
+            }
+            else
+            {
+                throw new Exception(response.ReasonPhrase);
+            }
+        }
+
+        private async Task CreateContact()
+        {
+            ContactModel contact = new ContactModel()
+            {
+                FirstName = "Megan",
+                LastName = "Hill"
+            };
+            contact.EmailAddresses.Add(new EmailAddressModel { EmailAddress = "blahbah" });
+            contact.PhoneNumbers.Add(new PhoneNumberModel { PhoneNumber = "3492023" });
+
+            var _client = _httpClientFactory.CreateClient();
+            var response = await _client.PostAsync("https://localhost:44379/api/contacts", new StringContent(JsonSerializer.Serialize(contact), Encoding.UTF8, "application/json"));
+        }
+    }
+}
---
+++ NoSqlDBSolution/MongoDBUI/Program.cs
@@ -0,0 +1,103 @@
+﻿using DataAccessLibrary;
+using DataAccessLibrary.Models;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace MongoDBUI
+{
+    public class Program
+    {
+        private static MongoDBDataAccess db;
+        private static readonly string tableName = "Contacts";
+
+        static void Main(string[] args)
+        {
+            db = new MongoDBDataAccess("MongoContactsDB", GetConnectionString());
+
+            ContactModel user = new ContactModel()
+            {
+                FirstName = "John",
+                LastName = "Berg"
+            };
+            user.EmailAddresses.Add(new EmailAddressModel { EmailAddress = "John@gmail.com" });
+            user.EmailAddresses.Add(new EmailAddressModel { EmailAddress = "dakoda3927@gmail.com" });
+            user.PhoneNumbers.Add(new PhoneNumberModel { PhoneNumber = "8506437584" });
+            user.PhoneNumbers.Add(new PhoneNumberModel { PhoneNumber = "6438941" });
+
+
+            //CreateContact(user);
+            //GetAllContacts();
+            //4fa0f34c-8765-49cc-b5d9-db9eeb4ba08d guid id
+            //GetContactById("4fa0f34c-8765-49cc-b5d9-db9eeb4ba08d");
+            //UpdateContactsFirstName("Crystal", "4fa0f34c-8765-49cc-b5d9-db9eeb4ba08d");
+            //GetAllContacts();
+            //RemovePhoneNumberFromUser("6438941", "4fa0f34c-8765-49cc-b5d9-db9eeb4ba08d");
+            //RemoveUser("4fa0f34c-8765-49cc-b5d9-db9eeb4ba08d");
+
+            Console.WriteLine("Done MongoDB");
+            Console.ReadLine();
+        }
+
+        private static void CreateContact(ContactModel contact)
+        {
+            db.UpsertRecord(tableName, contact.Id, contact);
+        }
+
+        private static void GetAllContacts()
+        {
+            var contacts = db.LoadRecords<ContactModel>(tableName);
+
+            foreach (var contact in contacts)
+            {
+                Console.WriteLine($"{contact.Id}: {contact.FirstName} {contact.LastName}");
+            }
+        }
+
+        private static void GetContactById(string id)
+        {
+            Guid guid = new Guid(id);
+            var contact = db.LoadRecordById<ContactModel>(tableName, guid);
+            Console.WriteLine($"{contact.Id}: {contact.FirstName} {contact.LastName}");
+        }
+
+        private static void UpdateContactsFirstName(string FirstName, string id)
+        {
+            Guid guid = new Guid(id);
+            var contact = db.LoadRecordById<ContactModel>(tableName, guid);
+            contact.FirstName = FirstName;
+            db.UpsertRecord(tableName, contact.Id, contact);
+        }
+
+        private static void RemovePhoneNumberFromUser(string phoneNumber, string id)
+        {
+            Guid guid = new Guid(id);
+            var contact = db.LoadRecordById<ContactModel>(tableName, guid);
+            contact.PhoneNumbers = contact.PhoneNumbers.Where(x => x.PhoneNumber != phoneNumber).ToList();
+            db.UpsertRecord(tableName, contact.Id, contact);
+        }
+
+        private static void RemoveUser(string id)
+        {
+            Guid guid = new Guid(id);
+            db.DeleteRecord<ContactModel>(tableName, guid);
+        }
+
+        private static string GetConnectionString(string connectionStringName = "Default")
+        {
+            string output = "";
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.Json");
+
+            var config = builder.Build();
+
+            output = config.GetConnectionString(connectionStringName);
+            return output;
+
+        }
+    }
+}
