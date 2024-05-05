336a2ad88db07ac092c3fa36784d3d0086a1e052
Dakoda Berg <dakodaberg97@gmail.com>
2024-05-05 01:49:55 -0400

    Dictionary

---
+++ DictionariesApp/Dictionaries/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+Dictionary<string, string> lookup = new Dictionary<string, string>();
+
+lookup["animal"] = "Not a human";
+lookup["fish"] = "Not a human that swims";
+lookup["human"] = "Us";
+
+Console.WriteLine($"The definitiion of fish is {lookup["fish"]}");
+
+Dictionary<int, string> employees = new Dictionary<int, string>();
+
+employees[1] = "Dakoda Berg";
+employees[2] = "John Berg";
+employees[44321] = "Tim Corey";
+
+Console.WriteLine($"The employee with ID number 2 is {employees[2]}");
+
+Dictionary<string, int> dayOfWeek = new Dictionary<string, int>();
+
+dayOfWeek["Wednesday"] = 4;
+dayOfWeek["Thursday"] = 5;
+dayOfWeek["Friday"] = 6;
+
+Console.WriteLine($"Wednesday is day number { dayOfWeek["Wednesday"]}");
---
+++ DictionaryHomeWorkApp/DictionaryHomeWork/Program.cs
@@ -0,0 +1,54 @@
+﻿
+//HomeWork
+//Create a Dictionary list of employee ID's and the name that goes with the ID.
+//Fill in a few records.
+//Then ask the user for their ID and return their name.
+//Use string for ID
+
+Dictionary<int, string> employees = new Dictionary<int, string>();
+
+employees[1] = "Dakoda";
+employees[2] = "John";
+
+
+//Console.Write("What is your employee ID number: ");
+//string? consoleResponse = Console.ReadLine();
+//Console.WriteLine($"Your name is {employees[consoleResponse]}.");
+
+//Ask user if they want to add employee(yes/no/done)
+//After adding new employee show all ID's and Names of all employees
+//If no then ask user for their employee ID
+
+
+Console.Write("Will you like to add an employee?(yes/no):");
+string addEmployee = Console.ReadLine();
+
+while (addEmployee.ToLower() == "yes")
+{
+    Console.Write("What is the new employees name?(type \"exit\" to quit): ");
+    string newName = Console.ReadLine();
+    if (newName == "exit")
+    {
+        Console.Write("Will you like to add an employee?(yes/no):");
+        addEmployee = Console.ReadLine();
+
+    }
+    else
+    {
+        employees.Add(employees.Count + 1, newName);
+        Console.WriteLine("Current employee list");
+        foreach (var pair in employees)
+        {
+            Console.WriteLine($"Employee ID = {pair.Key} Name = {pair.Value} ");
+        }
+
+    }
+}
+if (addEmployee.ToLower() == "no")
+{
+
+    Console.Write("What is your employee ID number: ");
+    string enteredIdText = Console.ReadLine();
+    bool isValidInt = int.TryParse(enteredIdText, out int employeeId);
+    Console.WriteLine($"Hello {employees[employeeId]}");
+}
