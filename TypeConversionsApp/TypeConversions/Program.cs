62db99f35da9f0ace015b37dd05c8202a7652393
Dakoda Berg <dakodaberg97@gmail.com>
2024-05-05 02:47:04 -0400

    Type Conversion

---
+++ TypeConversionsApp/TypeConversions/Program.cs
@@ -0,0 +1,39 @@
+﻿
+
+
+Console.Write("what is your age: ");
+
+string? ageText = Console.ReadLine();
+
+// taking what the user typed into the console 
+// and making it a string and not a number
+// so when you do ageText + 15 it won't add them together
+// so if ageText was 15 and we run it we would get 1515
+//Console.WriteLine(ageText + 15);
+
+// here we are making the ageText a int
+// so when we run age + 15 we actully add the age
+// so if ageText was 15, then we would get 30
+
+
+//int age = int.Parse(ageText);
+
+// caused a crash when trying edge cases
+// entered an age of "four"
+// Now using TryParse
+// Tryparse works with bool
+// out int age returns a int age
+bool isValidInt = int.TryParse(ageText, out int age);
+
+Console.WriteLine($"This is valid: {isValidInt}. The number was {age}.");
+
+
+//Console.WriteLine(age + 15 );
+
+// works because we aint losing in info
+// int can go into a double
+double testDouble = age;
+
+//casting a double to a decimal
+decimal testDecimal = (decimal)testDouble;
+
---
+++ TypeConversionsHomeWorkApp/TypeConversionsHomeWork/Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+// capture a users age from the console and
+// then identify how old they will be in 25 years,
+// as well as how old they were 25 years ago.
+// print that info to the console in natural language
+
+
+Console.WriteLine("Greeting! Will you like for me to tell you how old you'll be in 25 years");
+Console.Write("Please enter your current age: ");
+string? currentAge = Console.ReadLine();
+
+
+// saves the users age into one variable
+// also stops from crashing if invalid age is enter in console.
+// but if invalid age is enter it defaults age to 0.
+bool? isValidInt = int.TryParse(currentAge, out int age);
+
+// why save the user age into two different variable?
+// just save user age in one variable
+
+//int futureAge = int.Parse(currentAge);
+//int pastAge = int.Parse(currentAge);
+
+
+Console.WriteLine($"Great! In 25 years, you'll be {age + 25}!");
+Console.WriteLine($"Also 25 years ago you would have been {age - 25}!");
+
+
+
+
