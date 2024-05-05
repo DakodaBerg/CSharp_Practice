bbf51739dd8a85a026a9f77a9ce4b546e9767e62
Dakoda Berg <dakodaberg97@gmail.com>
2024-05-05 01:49:11 -0400

    Date Time Variable

---
+++ DateTimeVariablesApp/DateTimeVariables/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+using System.Globalization;
+
+DateTime today = DateTime.Now;
+
+//DateTime birthday = DateTime.Parse(s: "10/15/1997");
+
+DateTime birthday = DateTime.ParseExact("10/15/1997", "M/d/yyyy", CultureInfo.InvariantCulture);
+
+
+//Console.WriteLine(today.ToString(format:"T"));
+
+//Console.WriteLine(birthday);
+
+Console.WriteLine(today.ToString(format: "MMMM dd, yyyy hh:mm K tt zzz")); 
