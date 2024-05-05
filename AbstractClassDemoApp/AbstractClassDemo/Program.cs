bcbea31d8ad17eb52e76104f920ed371990f4588
Dakoda Berg <dakodaberg97@gmail.com>
2024-05-05 01:35:29 -0400

    Abstract Class

---
+++ AbstractClassDemoApp/AbstractClassDemo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            car.Manufactuer = "toyota";
+
+            Console.ReadLine();
+        }
+    }
+
+    public abstract class Vehicle
+    {
+        public int VIN { get; set; }
+        public string Manufactuer { get; set; }
+        public string Model { get; set; }
+        public int YearManufactured { get; set; }
+    }
+
+    public class Car : Vehicle
+    {
+        public int NumberOfWheels { get; set; } = 4;
+
+    }
+}
