7739762b043961a34bf9a7220623ada338f16397
Dakoda Berg <dakodaberg97@gmail.com>
2024-05-05 01:36:34 -0400

    Advanced If Statements

---
+++ AdvancedIfApp/AdvancedIf/Program.cs
@@ -0,0 +1,84 @@
+﻿
+//Console.Write("What is your first name: ");
+//string? firstName = Console.ReadLine();
+
+//Console.Write("What is your last Name: ");
+//string? lastName = Console.ReadLine();
+
+//if (firstName.ToLower() == "dakoda" &&
+//    lastName.ToLower() == "berg")
+//{
+//    Console.WriteLine("Hello Dakoda Berg!!!");
+//}
+//else if (firstName.ToLower() == "dakoda" ||
+//         lastName.ToLower() == "berg")
+//{
+//    Console.WriteLine("you have a great part in your name!");
+//}
+//else
+//{
+//    Console.WriteLine("Hello!");
+//}
+
+int age = 43;
+
+if ((age >= 40 && age < 50) || (age >= 70 && age < 80))
+{
+    Console.WriteLine("You are in your 40's or 70's");
+}
+
+
+
+
+
+//if (firstName.ToLower() == "dakoda" &&
+//    lastName.ToLower() == "berg")
+//{
+//    Console.WriteLine("Hello Dakoda Berg");
+//}
+//else if (firstName.ToLower() == "dakoda")
+//{
+//    Console.WriteLine("Hello you have a cool first name!!");
+//}
+//else if (lastName.ToLower() == "berg")
+//{
+//    Console.WriteLine("You have a cool last name!!");
+//}
+//else
+//{
+//    Console.WriteLine("Sorry you don't have cooler name!");
+//}
+
+//if (firstName.ToLower() == "dakoda")
+//{
+//    Console.WriteLine("you have a cool first name!");
+//}
+
+//if (lastName.ToLower() == "berg")
+//{
+//    Console.WriteLine("you have a cool last name!");
+//}
+
+//else
+//{
+//    Console.WriteLine("sorry your name isn't cooler");
+//}
+
+//int age = 0;
+
+//if (age > 100)
+//{
+//    Console.WriteLine("your up there in years!");
+//}
+
+//if (age <= 100)
+//{
+//    Console.WriteLine("you still have ways to go!");
+//}
+
+//if (age >= 40 && age < 50)
+//{
+//    Console.WriteLine("you are in your 40's");
+//}
+
+
