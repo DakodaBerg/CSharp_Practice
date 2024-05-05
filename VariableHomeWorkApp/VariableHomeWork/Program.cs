8d67a7bbde6134f2f839ca81593da7a5ff953347
Dakoda Berg <dakodaberg97@gmail.com>
2024-05-05 02:47:51 -0400

    Variable Practice

---
+++ VariableHomeWorkApp/VariableHomeWork/Program.cs
@@ -0,0 +1,21 @@
+﻿// Variable types homework
+// create a person name, age, if their alive, and phonenumber
+// using the best data types for each one.
+
+
+string? firstName = null;
+string? lastName = null;
+int? age = null;
+bool? personAlive = null;
+string? phoneNumber = null;
+
+firstName = "Dakoda";
+lastName = "Berg";
+age = 25;
+personAlive = true;
+phoneNumber = "(850) 643-8941";
+
+Console.WriteLine($"{firstName} {lastName}");
+Console.WriteLine(age);
+Console.WriteLine(personAlive);
+Console.WriteLine(phoneNumber);
