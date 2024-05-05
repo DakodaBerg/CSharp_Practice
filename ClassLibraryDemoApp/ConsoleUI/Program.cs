8d7c2f5c273de55fbc21b023327adb0706b19356
Dakoda Berg <dakodaberg97@gmail.com>
2024-05-05 01:48:20 -0400

    Class Library

---
+++ ClassLibraryDemoApp/ConsoleUI/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using CoreLibrary;
+
+class Program
+{
+    private static void Main(string[] args)
+    {
+        Generators generators = new Generators();
+        PersonModel person = new PersonModel()
+        {
+            Prefix = "Mr.",
+            FirstName = "Dakoda",
+            LastName = "Berg"
+        };
+
+        string message = generators.WelcomeMessage(person.Prefix, person.LastName);
+
+        Console.WriteLine(message);
+
+
+        Console.ReadLine();
+    }
+}
---
+++ ClassLibraryDemoApp/CoreLibrary/Generators.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreLibrary
+{
+
+    public class Generators
+    {
+        public string WelcomeMessage(string prefix, string lastName)
+        {
+            return $"Welcome To our demo app {prefix} {lastName}";
+        }
+
+        public string FarewellMessage(string prefix, string lastName)
+        {
+            return $"I hope you enjoyed the app {prefix} {lastName}. Please come back soon.";
+        }
+    }
+}
