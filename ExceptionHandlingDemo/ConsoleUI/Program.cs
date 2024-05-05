d396941e6d215a0a638bd69e65e982f11201a3e8
Dakoda Berg <dakodaberg97@gmail.com>
2024-05-05 02:29:07 -0400

    Exception Handling

---
+++ ExceptionHandlingDemo/ConsoleUI/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BadCall();
+            Console.ReadLine();
+        }
+
+        public static void BadCall()
+        {
+            int[] ages = { 1, 3, 5 };
+
+            for (int i = 0; i <= ages.Length; i++)
+            {
+                try
+                {
+                    Console.WriteLine(ages[i]);
+                }
+                catch (Exception ex)
+                {
+
+                    Console.WriteLine("We had an error");
+                    Console.WriteLine(ex.Message);
+                    throw;
+                }
+            }
+        }
+    }
+}
