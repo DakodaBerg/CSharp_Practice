074446279d268a5c1c0358bd97716cc43de3e540
Dakoda Berg <dakodaberg97@gmail.com>
2024-05-05 02:46:51 -0400

    Time Only Variable

---
+++ TimeOnlyVariablesApp/TimeOnlyVariables/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+
+//TimeOnly opensAt = TimeOnly.Parse("8:00 AM");
+using System.Globalization;
+
+TimeOnly opensAt = TimeOnly.Parse(s:"8:00 AM");
+TimeOnly now = TimeOnly.FromDateTime(DateTime.Now);
+
+
+
+Console.WriteLine(opensAt);
+
+// to format the time date ToString(format: " add how you want it formatted"));
+Console.WriteLine(now.ToString(format:"hh"));
+Console.WriteLine(timeOfBirth);
+
+
+
+
+
+
