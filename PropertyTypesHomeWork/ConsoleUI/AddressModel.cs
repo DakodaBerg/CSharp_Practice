5baff9666093210999335c0ca303d63d8b0ccf96
Dakoda Berg <dakodaberg97@gmail.com>
2024-05-05 02:43:57 -0400

    Property Types

---
+++ PropertyTypesHomeWork/ConsoleUI/AddressModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    public class AddressModel
+    {
+
+        public string StreetAddress { get; set; }
+        public string CityName { get; set; }
+        public string StateName { get; set; }
+        public string ZipCode { get; set; }
+
+
+        public string FullAddress
+        {
+            get
+            {
+                string fullAddress = $"The full adress is {StreetAddress} " +
+                    $"{CityName} " +
+                    $"{StateName} " +
+                    $"{ZipCode}";
+                return fullAddress;
+            }
+        }
+    }
+
+
+}
---
+++ PropertyTypesHomeWork/ConsoleUI/RequestAddressData.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    public class RequestAddressData
+    {
+
+        public static AddressModel GetAddress()
+        {
+            AddressModel address = new AddressModel();
+
+            Console.Write("What is your street address: ");
+            address.StreetAddress = Console.ReadLine();
+
+            Console.Write("What is your City Name: ");
+            address.CityName = Console.ReadLine();
+
+            Console.Write("What State are you located in: ");
+            address.StateName = Console.ReadLine();
+
+            Console.Write("What is your Zip Code: ");
+            address.ZipCode = Console.ReadLine();
+
+            return address;
+        }
+    }
+}
