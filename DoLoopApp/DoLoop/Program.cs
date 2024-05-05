39f1e9d1d6c02d60111b2f76e986b83938fa6527
Dakoda Berg <dakodaberg97@gmail.com>
2024-05-05 01:50:11 -0400

    Do Loop

---
+++ DoLoopApp/DoLoop/Program.cs
@@ -0,0 +1,73 @@
+﻿
+bool isValidAge;
+int age;
+//int testNumber = 0;
+
+
+
+//Console.Write("What is your age: ");
+//string? ageText = Console.ReadLine();
+
+//bool isValid = int.TryParse(ageText, out int age);
+
+
+//while (age < 21)
+//{
+//    Console.WriteLine("Your not old enought to buy beer!");
+//    return;
+//}
+
+//Console.WriteLine("Your old enought to buy beer!");
+
+//if (age < 21)
+//{
+//    Console.WriteLine("Your not old enough to buy beer");
+//}
+//else
+//{
+//    Console.WriteLine("You can buy beer!");
+//}
+
+
+//do
+//{
+//    Console.WriteLine(testNumber);
+//    testNumber += 3;
+//} while (testNumber < 10);
+
+
+// runs the code at least 1 time
+//do
+//{
+
+//} while (true);
+
+
+// runs the code 0 or more times
+//while (true)
+//{
+
+//}
+
+
+do
+{
+    Console.Write("What is your age: ");
+    string? ageText = Console.ReadLine();
+
+    isValidAge = int.TryParse(ageText, out age);
+
+    if (isValidAge == false)
+    {
+        Console.WriteLine("That was an invalid age.");
+
+    }
+} while (isValidAge == false);
+
+
+Console.WriteLine($"your age is {age}");
+
+
+
+
+
---
+++ DoLoopHomeWorkApp/DoLoopHomeWork/Program.cs
@@ -0,0 +1,52 @@
+﻿//HomeWork
+//create a console application
+//that asks the user for their name
+//welcome me (Tim) as professor or any one else as student
+//do this until the user types "exit.
+
+
+string? firstName;
+
+do
+{
+    Console.WriteLine("Enter your name or type exit to quit");
+    Console.Write("What is your name: ");
+    firstName = Console.ReadLine();
+
+
+    if (firstName.ToLower() == "tim")
+    {
+        Console.WriteLine("Hello Professor Tim!");
+    }
+    else if (firstName.ToLower() != "exit")
+    {
+        Console.WriteLine($"Hello student {firstName}");
+    }
+
+} while (firstName.ToLower() != "exit");
+
+
+
+
+//string? firstName = "";
+
+
+//while (firstName.ToLower() != "exit")
+//{
+
+
+//    Console.WriteLine("Enter your name or type exit to quit");
+//    Console.Write("What is your name: ");
+//    firstName = Console.ReadLine();
+
+
+//    if (firstName.ToLower() == "tim")
+//    {
+//        Console.WriteLine("Hello Professor Tim!");
+//    }
+//    else if (firstName.ToLower() != "exit")
+//    {
+//        Console.WriteLine($"Hello student {firstName}");
+//    }
+
+//}
