a28809b87a041bf7189c0cb1e872f68d94a0d090
Dakoda Berg <dakodaberg97@gmail.com>
2024-05-05 01:35:51 -0400

    Access Modifiers

---
+++ AccessModifiersDemoApp/AccessModifiersDemo/Program.cs
@@ -0,0 +1,44 @@
+﻿using DemoLibrary;
+using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessModifiersDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Person person = new Person();
+
+            person.FirstName = "Test";
+            person.SSN = "12345678"; 
+            
+
+            person.SayHello();
+
+            Console.ReadLine();
+        }
+    }
+
+    public class ModifiedDataAccess : DataAccess
+    {
+        public void GetUnsecureConnectInfo()
+        {
+            GetConnectionString();
+        }
+    }
+
+    public class CEO : Manager
+    {
+        public void GetConnectInfo()
+        {
+            DataAccess data = new DataAccess();
+            formerLastName = "dako";
+        }
+    }
+}
---
+++ AccessModifiersDemoApp/DemoLibrary/Person.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary
+{
+    public class DataAccess
+    {
+        protected internal string GetConnectionString()
+        {
+            return "returns Sensitive data";
+        }
+    }
+
+    public class Person
+    {
+        protected string formerLastName = "";
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+
+        private string _ssn;
+
+        public string SSN
+        {
+            get { return $"***-**-{_ssn.Substring(_ssn.Length - 4)}"; }
+            set { _ssn = value; }
+        }
+
+        public void ChangeLastName(string newLastName)
+        {
+            formerLastName = LastName;
+            LastName = newLastName;
+        }
+
+        public void SayHello()
+        {
+            Console.WriteLine($"Say Hello {FirstName} {SSN}");
+        }
+
+        public void SavePerson()
+        {
+            DataAccess dataAccess = new DataAccess();
+            string conn = dataAccess.GetConnectionString();
+            // saves the person
+        }
+    }
+
+    public class Employee : Person
+    {
+        public string GetFormerLastName()
+        {
+            return formerLastName;
+        }
+    }
+
+    public class Manager : Employee
+    {
+        public string GetAllNames()
+        {
+            return $"{FirstName},{LastName},{formerLastName}";
+        }
+    }
+}
