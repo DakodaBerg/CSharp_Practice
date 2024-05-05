9a7243f55f448687325c587041c59a94b6237e71
Dakoda Berg <dakodaberg97@gmail.com>
2024-05-05 02:40:38 -0400

    Null Variables

---
+++ NullVariablesApp/NullVariables/Program.cs
@@ -0,0 +1,34 @@
+﻿
+
+// null - lack of value
+
+
+// Haven't asked for age yet
+// ? allows for it to be null
+int? age = null;
+bool? birthday = null;
+double? gender = null;
+decimal? price = null;
+string? firstName = null;
+
+
+Console.WriteLine(age);
+Console.WriteLine(birthday);
+Console.WriteLine(gender);
+Console.WriteLine(price);
+Console.WriteLine(firstName);
+
+// We have asked for the age now
+age = 0;
+birthday = false;
+gender = 1.2;
+price = 89.0190190M;
+firstName = "Dakoda";
+
+
+Console.WriteLine(age);
+Console.WriteLine(birthday);
+Console.WriteLine(gender);
+Console.WriteLine(price);
+Console.WriteLine(firstName);
+
